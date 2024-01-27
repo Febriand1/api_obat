b87034da711ff4bf2aae7f9bc30af815ddb4b403
dirgafebrian <fdirga63@gmail.com>
2024-01-28 02:26:42 +0700

    Refactor code to use struct field instead of form value

--- obat.go
+++ obat.go
@@ -266,17 +266,17 @@ func InsertPenyakit(db *mongo.Database, col string, r *http.Request) (docs Penya
 	jenispenyakit := r.FormValue("jenis_penyakit")
 	namapenyakit := r.FormValue("nama_penyakit")
 	deskripsi := r.FormValue("deskripsi")
-	namaobat := r.FormValue("nama_obat")
 
 	objectID := primitive.NewObjectID()
 
+	var penyakit Penyakit
 	datapenyakit := bson.D{
 		{Key: "_id", Value: objectID},
 		{Key: "jenis_penyakit", Value: jenispenyakit},
 		{Key: "nama_penyakit", Value: namapenyakit},
 		{Key: "deskripsi", Value: deskripsi},
 		{Key: "obat", Value: bson.D{
-			{Key: "nama_obat", Value: namaobat},
+			{Key: "nama_obat", Value: penyakit.Obat.Nama_Obat},
 		}},
 	}
 
@@ -297,8 +297,8 @@ func UpdatePenyakit(db *mongo.Database, col string, _id primitive.ObjectID, r *h
 	jenispenyakit := r.FormValue("jenis_penyakit")
 	namapenyakit := r.FormValue("nama_penyakit")
 	deskripsi := r.FormValue("deskripsi")
-	namaobat := r.FormValue("nama_obat")
 
+	var penyakit Penyakit
 	filter := bson.M{"_id": _id}
 
 	update := bson.D{
@@ -307,7 +307,7 @@ func UpdatePenyakit(db *mongo.Database, col string, _id primitive.ObjectID, r *h
 			{Key: "nama_penyakit", Value: namapenyakit},
 			{Key: "deskripsi", Value: deskripsi},
 			{Key: "obat", Value: bson.D{
-				{Key: "nama_obat", Value: namaobat},
+				{Key: "nama_obat", Value: penyakit.Obat.Nama_Obat},
 			}},
 		}},
 	}
@@ -381,8 +381,8 @@ func InsertRS(db *mongo.Database, col string, r *http.Request) (docs RumahSakit,
 	alamat := r.FormValue("alamat")
 	latitude := r.FormValue("latitude")
 	longitude := r.FormValue("longitude")
-	namaobat := r.FormValue("nama_obat")
 
+	var penyakit Penyakit
 	objectID := primitive.NewObjectID()
 
 	imageUrl, err := image.SaveFileToGithub("Febriand1", "fax.mp4@gmail.com", "Image", "pemrog", r)
@@ -399,7 +399,7 @@ func InsertRS(db *mongo.Database, col string, r *http.Request) (docs RumahSakit,
 		{Key: "longitude", Value: longitude},
 		{Key: "gambar", Value: imageUrl},
 		{Key: "obat", Value: bson.D{
-			{Key: "nama_obat", Value: namaobat},
+			{Key: "nama_obat", Value: penyakit.Obat.Nama_Obat},
 		}},
 	}
 
@@ -423,7 +423,6 @@ func UpdateRS(db *mongo.Database, col string, _id primitive.ObjectID, r *http.Re
 	latitude := r.FormValue("latitude")
 	longitude := r.FormValue("longitude")
 	gambar := r.FormValue("file")
-	namaobat := r.FormValue("nama_obat")
 
 	if gambar != "" {
 		imageUrl = gambar
@@ -435,6 +434,7 @@ func UpdateRS(db *mongo.Database, col string, _id primitive.ObjectID, r *http.Re
 		gambar = imageUrl
 	}
 
+	var penyakit Penyakit
 	filter := bson.M{"_id": _id}
 
 	update := bson.D{
@@ -446,7 +446,7 @@ func UpdateRS(db *mongo.Database, col string, _id primitive.ObjectID, r *http.Re
 			{Key: "longitude", Value: longitude},
 			{Key: "gambar", Value: gambar},
 			{Key: "obat", Value: bson.D{
-				{Key: "nama_obat", Value: namaobat},
+				{Key: "nama_obat", Value: penyakit.Obat.Nama_Obat},
 			}},
 		}},
 	}
